7b53c70a92077c33af3f8f8273dc987abd1e708b
mhendrick98 <mhendric@bu.edu>
2020-04-09 13:55:00 -0700

    pushed

--- src/main/test/DBSCANTests.java
+++ src/main/test/DBSCANTests.java
@@ -1,6 +1,14 @@
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.desilvahendricksoftware.debs.DBSCAN;
+import org.desilvahendricksoftware.debs.Point;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 
@@ -9,28 +17,28 @@ public class DBSCANTests {
     private DBSCAN testScan = new DBSCAN(0.5, 3);
 
     // cluster 0
-    private final DBSCAN.Point p1 = new DBSCAN.Point(1.1, 1.23);
-    private final DBSCAN.Point p2 = new DBSCAN.Point(1.34, 1.54);
-    private final DBSCAN.Point p3 = new DBSCAN.Point(1.01, 1.13);
-    private final DBSCAN.Point p4 = new DBSCAN.Point(1.03, 1.24);
-    private final DBSCAN.Point p5 = new DBSCAN.Point(1.56, 1.89);
+    private final Point p1 = new Point(1.1, 1.23);
+    private final Point p2 = new Point(1.34, 1.54);
+    private final Point p3 = new Point(1.01, 1.13);
+    private final Point p4 = new Point(1.03, 1.24);
+    private final Point p5 = new Point(1.56, 1.89);
 
     // cluster 1
-    private final DBSCAN.Point p6 = new DBSCAN.Point(4.0, 7.0);
-    private final DBSCAN.Point p7 = new DBSCAN.Point(4.02, 7.08);
-    private final DBSCAN.Point p8 = new DBSCAN.Point(4.03, 7.02);
-    private final DBSCAN.Point p9 = new DBSCAN.Point(4.04, 7.04);
+    private final Point p6 = new Point(4.0, 7.0);
+    private final Point p7 = new Point(4.02, 7.08);
+    private final Point p8 = new Point(4.03, 7.02);
+    private final Point p9 = new Point(4.04, 7.04);
 
     // noise
-    private final DBSCAN.Point p10 = new DBSCAN.Point(23, 19);
+    private final Point p10 = new Point(23, 19);
 
     @Test
     public void testCalculateDistance() throws Exception {
 
-        assertEquals(5.0, DBSCAN.calculateDistance(new DBSCAN.Point(1.0, 3.0), new DBSCAN.Point(4.0, 7.0)), 0.0);
-        assertEquals(118.6, DBSCAN.calculateDistance(new DBSCAN.Point(23.0, -43.0), new DBSCAN.Point(94.0,-138.0)), 0.001);
-        assertEquals(0.0, DBSCAN.calculateDistance(new DBSCAN.Point(0.0, 0.0), new DBSCAN.Point(0.0, 0.0)), 0.0);
-        assertEquals(34.195, DBSCAN.calculateDistance(new DBSCAN.Point(9.23,0.35), new DBSCAN.Point(42.345,8.88)), 0.001);
+        assertEquals(5.0, DBSCAN.calculateDistance(new Point(1.0, 3.0), new Point(4.0, 7.0)), 0.0);
+        assertEquals(118.6, DBSCAN.calculateDistance(new Point(23.0, -43.0), new Point(94.0,-138.0)), 0.001);
+        assertEquals(0.0, DBSCAN.calculateDistance(new Point(0.0, 0.0), new Point(0.0, 0.0)), 0.0);
+        assertEquals(34.195, DBSCAN.calculateDistance(new Point(9.23,0.35), new Point(42.345,8.88)), 0.001);
 
         assertEquals(0.392, DBSCAN.calculateDistance(p1, p2), 0.001);
         assertEquals(0.1118, DBSCAN.calculateDistance(p3, p4), 0.001);
@@ -49,8 +57,7 @@ public class DBSCANTests {
 
     @Test
     public void testGetNeighbors() throws Exception {
-
-        DBSCAN.Point [] points = new DBSCAN.Point[] {p1, p2, p3, p4, p5, p6};
+         Point [] points = new Point[] {p1, p2, p3, p4, p5, p6};
 
         assertEquals(true, testScan.getNeighbors(p1, points).contains(p2));
         assertEquals(true, testScan.getNeighbors(p1, points).contains(p3));
@@ -63,8 +70,7 @@ public class DBSCANTests {
     }
 
     @Test
-    public void testFit() throws Exception {
-        DBSCAN.Point [] points = new DBSCAN.Point[] {p1, p10, p8, p3, p4, p5, p6, p7, p2, p9};
+    public void testFit() throws Exception {  Point [] points = new Point[] {p1, p10, p8, p3, p4, p5, p6, p7, p2, p9};
 
         testScan.fit(points);
 
@@ -97,6 +103,29 @@ public class DBSCANTests {
         assertEquals("With an epsilon of 0.20 and a minimum # of points of 20, DBSCAN should return 5 clusters for a dataset containing 5 true clusters.",
                 5,
                 dbscan.numberOfClustersWithoutNoiseCluster);
+    }
+
+    @Test
+    public void DBSCANMergeRightToLeftCollectionTest(){
+        DBSCAN dbscan = new DBSCAN(0.20, 20);
+        ArrayList<Point> input1 = new ArrayList<>();
+        ArrayList<Point> input2 = new ArrayList<>();
+        ArrayList<Point> expected = new ArrayList<>();
+        input1.add(new Point(1.0,2.0));
+        input1.add(new Point(1.1,2.1));
+        input1.add(new Point(1.2,2.2));
+        input1.add(new Point(1.3,2.3));
+        input2.add(new Point(2.0,3.0));
+        input2.add(new Point(2.1,3.1));
+        input2.add(new Point(1.2,2.2));
+        input2.add(new Point(1.3,2.3));
+        expected.add(new Point(1.0,2.0));
+        expected.add(new Point(1.1,2.1));
+        expected.add(new Point(1.2,2.2));
+        expected.add(new Point(1.3,2.3));
+        expected.add(new Point(2.0,3.0));
+        expected.add(new Point(2.1,3.1));
+        assertArrayEquals(expected.toArray(), dbscan.mergeRightToLeftCollection(input1,input2).toArray());
     }
 
     public Point[] readSamplesFromCsv(String filepath) {
